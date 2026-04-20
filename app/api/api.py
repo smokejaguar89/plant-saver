512c47b00481907d6665262137a0e16ba68beda2
Nicholas Hall <hall.james.nicholas@gmail.com>
2026-04-20 12:57:24 +0200

    remove arbitrary wait - let's see if it works without

--- app/api/api.py
+++ app/api/api.py
@@ -93,10 +93,6 @@ async def update_cron_time():
     BLOOMIN8_URL = "http://192.168.86.241/upstream/pull_settings"
     GET_IMAGE_URL = "http://192.168.86.26:8000/api/images"
 
-    # Wait 5 minutes. The frame needs time to take the image and update itself,
-    # during which time it may be blocked.
-    await asyncio.sleep(300)
-
     next_pull_time = await get_next_pull_time()
 
     # .strftime('%Y-%m-%dT%H:%M:%SZ') is the safest way to ensure the 'Z' suffix
